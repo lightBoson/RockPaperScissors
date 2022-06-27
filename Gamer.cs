c8fe360ccaba73377f6354f5354ed173bd8e9301
karolinajoanna <karolina.j.osuch@gmail.com>
2022-06-27 20:35:28 +0200

    first commit

---
+++ Gamer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperStoneScissors
+{
+    enum GamerType
+    {
+        Human,
+        Computer
+    }
+
+
+    internal class Gamer
+    {
+        //Atributes 
+        private string _name;
+        private GamerType _type;
+        private int _bestResult;
+
+
+        //Methods
+        public string getName() { return _name;}
+        public int getBestResult() { return _bestResult;}
+        public void setName(string newName) { _name = newName; }
+        public GamerType getType() { return _type; }
+
+        public Answer randomChoice() {
+            Random random = new Random();
+            return (Answer)random.Next(Enum.GetNames(typeof(Answer)).Length); }
+        public Gamer(string name, GamerType type = GamerType.Computer)
+        {
+            _name = name;
+            _type = type;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿namespace PaperStoneScissors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Welcome the Gamer
+            Welcome();
+            Gamer human = CreateUser(); 
+            int gamePlayNumber = 0;
+            ConsoleKeyInfo info = Console.ReadKey(true);
+            while (info.Key != ConsoleKey.Escape)
+            {
+                Gamer computer = new(RandomGamerName(), GamerType.Computer);
+                Gameplay gameplay = new Gameplay(human, computer, gamePlayNumber++);
+                Result result = gameplay.startGameplay();
+
+                if (result == Result.FirstWins)
+                {
+                    Console.WriteLine("Congratulations " + human.getName() + "!");
+                }
+                else if (result == Result.SecondWins)
+                {
+                    Console.WriteLine("Congratulations " + computer.getName() + "!");
+                }
+                else
+                {
+                    Console.WriteLine("Draw! Nice try, both of you :)");
+                }
+                Console.WriteLine("If you want to quit press ESCAPE, to continue press any other button");
+                info = Console.ReadKey();
+
+            }
+
+
+        }
+        static void Welcome() {
+            Console.WriteLine("Welcome to the game!");
+        }
+        static Gamer CreateUser()
+        {
+            string userName = "";
+            while (string.IsNullOrEmpty(userName))
+            {
+                Console.WriteLine("Please provide your name:");
+                userName = Console.ReadLine();
+            }
+
+            Gamer human = new(userName, GamerType.Human); 
+            // Welcome new human gamer
+            Console.WriteLine("Good luck " + userName+"!");    
+
+            return human;
+        }
+
+        static string RandomGamerName()
+        {
+            List<string> names = new List<string>() { "Adam", "Robert", "Pierre", "Lucile", "Eva", "Kate" };
+            var random = new Random();
+            int index = random.Next(names.Count);
+            return names[index];
+        }
+    }
+
+}
